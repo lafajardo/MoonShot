bf9e2e15d31697bb90858bdee6d925dabb336691
lafajardo <lfajardo@unc.edu>
2024-04-11 15:55:52 -0400

    complete

---
+++ moonshot/moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  moonshot
+//
+//  Created by Luis Fajardo on 4/11/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ moonshot/moonshot/moonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonshotApp.swift
+//  moonshot
+//
+//  Created by Luis Fajardo on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct moonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
